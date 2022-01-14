72e63418a15dc1457c8d69450d08dac0200d5f9c
JordanChoi <s10208161@connect.np.edu.sg>
2022-01-14 22:25:02 +0800

    Initial Commit

---
+++ Kamaemon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kamaemon
+//
+//  Created by Jordan Choi on 14/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
