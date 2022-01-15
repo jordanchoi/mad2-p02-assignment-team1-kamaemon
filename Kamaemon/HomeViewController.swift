ea7b2e9b0de9bae0d430c5f3511073b680df82cb
Jun Hong <s10203330@connect.np.edu.sg>
2022-01-16 01:32:18 +0800

    Implemented Log In From Firebase Authentication

---
+++ Kamaemon/HomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewController.swift
+//  Kamaemon
+//
+//  Created by Jun Hong on 16/1/22.
+//
+
+import Foundation
+import UIKit
+import FirebaseAuth
+
+class HomeViewController : UIViewController{
+    
+    @IBOutlet weak var user: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let userInfo = Auth.auth().currentUser
+        let email = userInfo?.email
+        user.text = email
+        
+        print("Email: \(email)")
+
+        
+    }
+    
+    
+    
+}
--- Kamaemon/ViewController.swift
+++ Kamaemon/ViewController.swift
@@ -6,14 +6,61 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var EmailAddress: UITextField!
+    
+    @IBOutlet weak var Password: UITextField!
+    
+    @IBOutlet weak var newUser: UILabel!
+    @IBOutlet weak var errorlabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+//        Auth.auth().createUser(withEmail: "honglec162003@gmail.com", password: "123456789") { authResult, error in
+//
+//            print("success")
+//        }
+        
+        //let tap = UITapGestureRecognizer(target: self, action: Selector())
+        
     }
 
-
+    @IBAction func LogIn(_ sender: Any) {
+        
+        Auth.auth().signIn(withEmail: EmailAddress!.text!, password: Password!.text!) { (authResult, error) in
+            if let error = error as? NSError {
+               switch AuthErrorCode(rawValue: error.code) {
+               case .wrongPassword:
+                   self.errorlabel.text = "Wrong Password"
+               case .invalidEmail:
+                   self.errorlabel.text = "Invalid Email"
+                 // Error: Indicates the email address is malformed.
+               default:
+                   print("Error: \(error.localizedDescription)")
+               }
+             } else {
+               print("User signs in successfully")
+               let userInfo = Auth.auth().currentUser
+               let email = userInfo?.email
+               let storyboard = UIStoryboard(name: "Main", bundle: nil)
+               let home = storyboard.instantiateViewController(withIdentifier: "home") as! UIViewController
+                 home.modalPresentationStyle = .fullScreen
+                 self.present(home, animated: true, completion: nil)
+                
+             }
+            
+            
+            
+        }
+    }
+    
+    
+   
+    
 }
 
