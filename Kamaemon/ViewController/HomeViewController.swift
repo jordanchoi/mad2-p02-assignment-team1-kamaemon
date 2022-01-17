cc5e3035334fdf541ace090f5a5976ca3efba1be
Jun Hong <s10203330@connect.np.edu.sg>
2022-01-17 16:51:46 +0800

    Added get name from database and create user and write to database in register and home

--- Kamaemon/ViewController/HomeViewController.swift
+++ Kamaemon/ViewController/HomeViewController.swift
@@ -8,17 +8,37 @@
 import Foundation
 import UIKit
 import FirebaseAuth
-
+import Firebase
 class HomeViewController : UIViewController{
     
     @IBOutlet weak var user: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.\
-        let displayName = "Balqris"
+        var ref: DatabaseReference!
+        ref = Database.database(url: "https://kamaemon-default-rtdb.asia-southeast1.firebasedatabase.app/").reference()
+//        ref.child("users").child(Auth.auth().currentUser!.uid).child("Name").observeSingleEvent(of: .value) { snapshot in
+//            guard let value1 = snapshot.value as? String else{
+//                return
+//            }
+//            displayName = value1
+//
+//        }
+            //.value(forKeyPath: "Name") as! String?
+        print(Auth.auth().currentUser!.uid)
+        ref.child("users").child(Auth.auth().currentUser!.uid).observeSingleEvent(of: .value, with: { snapshot in
+          // Get user value
+          let value = snapshot.value as? NSDictionary
+            let displayName = value?["Name"] as? String ?? "Error"
+            self.user.text = "Hello, " + displayName + " 👋"
+          //let user = User(username: username)
+
+          // ...
+        }) { error in
+          print(error.localizedDescription)
+        }
         
         
-        user.text = "Hello, " + displayName + " 👋"
         
 
 
--- Kamaemon/ViewController/RegisterPageViewController.swift
+++ Kamaemon/ViewController/RegisterPageViewController.swift
@@ -8,7 +8,7 @@
 import Foundation
 import UIKit
 import Firebase
-
+import FirebaseAuth
 class RegisterPageViewController : UIViewController {
         
     @IBOutlet weak var Name: UITextField!
@@ -38,14 +38,29 @@ class RegisterPageViewController : UIViewController {
     
     @IBAction func createAccount(_ sender: Any) {
         
+    
+//        do {
+//            try Auth.auth().signOut()
+//        } catch let error {
+//            print("(error)")
+//        }
         Auth.auth().createUser(withEmail: EmailAddress.text!, password: Password.text!) { authResult, error in
             print("success")
+            print(authResult?.user.uid)
+            let u = User(userUID: (authResult?.user.uid)!, userCategory: self.cat.currentTitle!, name: self.Name.text!)
+            var ref: DatabaseReference!
+            ref = Database.database(url: "https://kamaemon-default-rtdb.asia-southeast1.firebasedatabase.app/").reference()
+            ref.child("users").child((authResult?.user.uid)!).setValue(["userUID" :(authResult?.user.uid)!, "userCategory" : u.Category, "Name" : u.n])
         }
-        let u = User(userUID: Auth.auth().currentUser!.uid, userCategory: cat.currentTitle!, name: Name.text!)
-        var ref: DatabaseReference!
-        ref = Database.database(url: "https://kamaemon-default-rtdb.asia-southeast1.firebasedatabase.app/").reference()
-        ref.child("users").child(Auth.auth().currentUser!.uid).setValue(["userUID" : u.UID, "userCategory" : u.Category, "Name" : u.n])
+        
+    
+    
+        
     }
+
     
     
 }
+    
+    
+    
