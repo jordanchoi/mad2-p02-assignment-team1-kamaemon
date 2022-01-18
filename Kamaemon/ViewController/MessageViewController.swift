0ddc053be8132cdf45b4e1cb188b4c8001895fbb
Jun Hong <s10203330@connect.np.edu.sg>
2022-01-19 02:55:29 +0800

    Added loadMessages() action

--- Kamaemon/ViewController/MessageViewController.swift
+++ Kamaemon/ViewController/MessageViewController.swift
@@ -9,10 +9,12 @@ import Foundation
 import UIKit
 import Firebase
 import FirebaseAuth
+import SwiftProtobuf
 
 class MessageViewController : UITableViewController{
     // Data model: These strings will be the data for the table view cells
     let animals: [String] = ["Horse", "Cow", "Camel", "Sheep", "Goat"]
+    let messages : [Message] = []
     var chosenuser : User?{
         didSet{
             navigationItem.title = chosenuser?.n
@@ -53,7 +55,7 @@ class MessageViewController : UITableViewController{
         ref = Database.database(url: "https://kamaemon-default-rtdb.asia-southeast1.firebasedatabase.app/").reference()
         let message = Message(Messageto: String(chosenuser!.UID), Messagefrom: Auth.auth().currentUser!.uid, m: "supposedly from text field")
         //ref.child("users").child((authResult?.user.uid)!).setValue(["userUID" :(authResult?.user.uid)!, "userCategory" : u.Category, "Name" : u.n])
-        ref.child("Messages").childByAutoId().setValue(["Messageto" : message.MessageTo, "MessageFrom" : message.MessageFrom, "Message" : message.Message])
+        ref.child("Messages").childByAutoId().setValue(["MessageTo" : message.MessageTo, "MessageFrom" : message.MessageFrom, "Message" : message.Message])
     
         
     }
@@ -61,11 +63,32 @@ class MessageViewController : UITableViewController{
     
     
     func loadMessages(){
+        var ref: DatabaseReference!
+        ref = Database.database(url: "https://kamaemon-default-rtdb.asia-southeast1.firebasedatabase.app/").reference().child("Messages")
+        
+        ref.observeSingleEvent(of: .value, with: { snapshot in
+          // Get user value
+            let value = snapshot.value as? [String: AnyObject]
+            for i in value!.keys{
+                print(value![i]!["Messageto"] as! String)
+                let m = Message(Messageto: value![i]!["MessageTo"] as! String, Messagefrom: value![i]!["MessageFrom"] as! String, m: value![i]!["Message"] as! String)
+                if (m.MessageTo == chosenuser!.UID  && m.MessageFrom == Auth.auth().currentUser!.uid || m.MessageTo = Auth.auth().currentUser!.uid && m.MessageFrom == chosenuser!.UID){
+                    messages.append(m)
+                }
+            }
+            DispatchQueue.global(qos: .background).async {
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
+
+           
+        
+        }){ error in
+            print(error.localizedDescription)
+          }
         
         
-        
-    }
-    
     
     func upcomingMessages(){
         
@@ -73,4 +96,5 @@ class MessageViewController : UITableViewController{
         
     }
 
+}
 }
