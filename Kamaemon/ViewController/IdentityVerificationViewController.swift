7b96c48b91ed856692cda3dbc982562421625bc9
JordanChoi <s10208161@connect.np.edu.sg>
2022-01-21 14:16:12 +0800

    Implemented functionality to allow users to capture selfie image after capturing NRIC. Buttons will be disabled after capture. Implemented functionality to upload captured images to FirebaseStorage.
    To implement Microsoft Azure Biometrics

--- Kamaemon/ViewController/IdentityVerificationViewController.swift
+++ Kamaemon/ViewController/IdentityVerificationViewController.swift
@@ -7,22 +7,43 @@
 
 import Foundation
 import UIKit
+import SwiftUI
+import FirebaseStorage
 
-class IdentityVerificationViewController: UIViewController {
-    @IBOutlet weak var testImgView: UIImageView!
+class IdentityVerificationViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+    
+    // View Outlets
+    @IBOutlet weak var testImgView: UIImageView!    // to remove for testing purpose only
     @IBOutlet weak var uploadBtn: UIButton!
+    @IBOutlet weak var selfieBtn: UIButton!
+    
+    // Firebase Storage
+    private let storage = Storage.storage().reference()
+    
+    // Flag to use the same delegate function for capturing images
+    var forNric:Bool = true
+    
+    // Image of NRIC captured by user
+    var nric:UIImage?
+    var nricBytes:Data? = nil
+    
+    // Image of Selfie captured by user
+    var selfie:UIImage?
+    var selfieBytes:Data? = nil
+    
+    // Images shall be uploaded to Firebase and Microsoft Azure Face Biometrics API
+    // Selfie captured will be used as Profile Image
     
     override func viewDidLoad() {
         super.viewDidLoad()
         uploadBtn.cornerRadius = 10
-        
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(true)
     }
     
-    @IBAction func uploadNRIC() {
+    @IBAction func captureImg() {
         let picker = UIImagePickerController()
         picker.sourceType = .camera
         picker.delegate = self
@@ -31,11 +52,46 @@ class IdentityVerificationViewController: UIViewController {
     }
     
     @IBAction func confirmReg(_ sender: Any) {
+        // upload images to FirebaseStorage
+        if (nricBytes != nil && selfieBytes != nil) {
+            let name = "Jordan" // for testing to remove
+            let nricRef = storage.child("images/nric/\(name)_nric.png")
+            let selfieRef = storage.child("images/selfie/\(name)_selfie.png")
+            
+            // nric image
+            nricRef.putData(nricBytes!, metadata: nil, completion: {_, error in
+                guard error == nil else {
+                    print("Upload Failed - NRIC")
+                    return
+                }
+                nricRef.downloadURL(completion: {url, error in
+                                    guard let url = url, error == nil else {
+                    return
+                }
+                                    let urlStr = url.absoluteString
+                                    print("Download URL: \(urlStr)")
+                                    UserDefaults.standard.set(urlStr, forKey: "url")
+                })
+            })
+            
+            selfieRef.putData(selfieBytes!, metadata: nil, completion: {_, error in
+                guard error == nil else {
+                    print("Upload Failed - Selfie")
+                    return
+                }
+                selfieRef.downloadURL(completion: {url, error in
+                                    guard let url = url, error == nil else {
+                    return
+                }
+                                    let urlStr = url.absoluteString
+                                    print("Download URL: \(urlStr)")
+                                    UserDefaults.standard.set(urlStr, forKey: "url")
+                })
+            })
+        }
+        
     }
     
-}
-
-extension IdentityVerificationViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         picker.dismiss(animated: true, completion: nil)
     }
@@ -46,7 +102,21 @@ extension IdentityVerificationViewController: UIImagePickerControllerDelegate, U
         guard let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage else {
             return
         }
-        testImgView.image = image
+        
+        if (forNric) {
+            nric = image
+            forNric = false;
+            nricBytes = image.pngData()
+            testImgView.image = nric; // for testing to remove
+            uploadBtn.isEnabled = false
+        }
+        else {
+            selfie = image
+            selfieBytes = image.pngData()
+            forNric = true;
+            testImgView.image = selfie // for testing to remove
+            selfieBtn.isEnabled = false
+        }
     }
 }
 
