ed77bd6212fbd74ce5df44e6861c5900e5c946a3
Jun Hong <s10203330@connect.np.edu.sg>
2022-01-22 01:54:29 +0800

    Added getCurrentUser in DAL

--- Kamaemon/Firebase DAL/userDAL.swift
+++ Kamaemon/Firebase DAL/userDAL.swift
@@ -44,14 +44,37 @@ class userDAL{
     }
     
     
-    func createNewUser(newUser : User) {
-        
-        
+    //create new user
+    func createNewUser(newUser : User){
         ref.child("users").child(newUser.UID).setValue(["userUID" :(newUser.UID), "userType" : newUser.UserType, "Name" : newUser.n, "Gender" : newUser.Gender, "PhoneNumber" : newUser.PhoneNumber, "BirthDate" : newUser.BirthDate, "ProfilePicURL" : newUser.profilepicurl, "IsNewUser" : newUser.isNewUser])
-        
         
     }
     
+    //value returned through parameters
+    func getCurrentUser(UserUID : String, currentuser: User){
+        //var user : User
+        ref.child("users").child(UserUID).observeSingleEvent(of: .value) { DataSnapshot in
+            let value = DataSnapshot.value as? NSDictionary
+            let userUID = value?["userUID"] as? String
+            let userType = value?["userType"] as? String
+            let Name = value?["Name"] as? String
+            let Gender = value?["Gender"] as? String
+            let PhoneNumber = value?["PhoneNumber"] as? String
+            let BirthDate = value?["BirthDate"] as? Date
+            let pfpurl = value?["ProfilePicURL"] as? String
+            let IsNewUser = value?["IsNewUser"] as? Int
+            currentuser.UID = userUID!
+            currentuser.UserType = userType!
+            currentuser.n = Name!
+            currentuser.Gender = Gender!
+            currentuser.PhoneNumber = PhoneNumber!
+            currentuser.BirthDate = BirthDate!
+            currentuser.profilepicurl = pfpurl!
+            currentuser.isNewUser = IsNewUser!
+            
+        }
+       // return user
+    }
     
     
 }
